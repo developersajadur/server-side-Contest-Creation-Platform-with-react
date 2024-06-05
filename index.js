52c5229abe23e8a6df38406b2e7606066b9e5491
itzmesojib <itzmesojib@gmail.com>
2024-06-05 23:04:47 +0600

    stripe payment method add

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
@@ -29,6 +30,7 @@ client.connect().then(() => {
   // Database collections
   const ContestCollections = client.db("ContestCreationDb").collection("contests");
   const UserCollections = client.db("ContestCreationDb").collection("users");
+  const PaymentCollections = client.db("ContestCreationDb").collection("payments");
 
   // JWT token generation endpoint
   app.post("/jwt", async (req, res) => {
@@ -36,7 +38,7 @@ client.connect().then(() => {
     const token = jwt.sign(user, process.env.JWT_ACCESS_TOKEN, {
       expiresIn: "7d",
     });
-    res.send(token);
+    res.send({ token });
   });
 
   // Token verification middleware
@@ -45,7 +47,7 @@ client.connect().then(() => {
     if (!token) {
       return res.status(401).send({ message: "Unauthorized Access denied" });
     }
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    jwt.verify(token, process.env.JWT_ACCESS_TOKEN, (err, decoded) => {
       if (err) {
         return res.status(401).send({ message: "Unauthorized Access denied" });
       }
@@ -54,24 +56,54 @@ client.connect().then(() => {
     });
   };
 
+  // Stripe payment related API
+  app.post("/create-payment-intent", async (req, res) => {
+    // console.log(res);
+    const { price } = req.body;
+    const amount = parseInt(price * 100); // Convert to cents
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: "usd",
+        payment_method_types: ["card"],
+    });
+
+    res.send({
+        clientSecret: paymentIntent.client_secret,
+    });
+});
+
+// add payment transaction
+ app.post("/payment", async (req, res) => {
+  const newPayment = req.body;
+  const result = await PaymentCollections.insertOne(newPayment);
+  res.send(result);
+ })
+//  get payment transaction
+ app.get("/payment", async (req, res) => {
+  const allPayment = await PaymentCollections.find().toArray();
+  res.send(allPayment);
+ })
+
+
   // Post a contest
   app.post("/contests", async (req, res) => {
     const newContest = req.body;
-    const result = await ContestCollections.insertOne(newContest);
+    const result = await ContestCollections.insertOne(newContest)
     res.send(result);
   });
 
   // Get all contests
   app.get("/contests", async (req, res) => {
-    const allContests = await ContestCollections.find().toArray();
-    res.send(allContests);
+    const result = await ContestCollections.find().toArray()
+    res.send(result);
   });
 
   // Delete a contest
-  app.delete("/contests/:id",  async (req, res) => {
+  app.delete("/contests/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
-    const result = await ContestCollections.deleteOne(query);
+    const result = await ContestCollections.deleteOne(query)
     res.send(result);
   });
 
@@ -93,7 +125,7 @@ client.connect().then(() => {
         contestDeadline: contestData.contestDeadline,
       }
     };
-    const result = await ContestCollections.updateOne(filter, post, options);
+    const result = await ContestCollections.updateOne(filter, post, options)
     res.send(result);
   });
 
@@ -101,73 +133,80 @@ client.connect().then(() => {
   app.get("/contests/:contestName", async (req, res) => {
     const contestName = req.params.contestName;
     const query = { contestName };
-    const result = await ContestCollections.findOne(query);
+    const result = await ContestCollections.findOne(query)
     res.send(result);
   });
-  // get a contest by id
+
+  // Get a contest by id
   app.get("/contest/:id", async (req, res) => {
     const id = req.params.id;
     const query = { _id: new ObjectId(id) };
-    const result = await ContestCollections.findOne(query);
+    const result = await ContestCollections.findOne(query)
     res.send(result);
-  });
+  }); 
 
   // ------------ users related data --------------------
   // Post a user
   app.post("/users", async (req, res) => {
     const newUser = req.body;
-    const result = await UserCollections.insertOne(newUser);
+    const result = await UserCollections.insertOne(newUser)
     res.send(result);
   });
-  // get all users
+
+  // Get all users
   app.get("/users", async (req, res) => {
-    const allUsers = await UserCollections.find().toArray();
-    res.send(allUsers);
+    const result = await UserCollections.find().toArray()
+    res.send(result);
   });
-      // --------------- make a user admin ---------------
-      app.patch("/users/admin/:id", async (req,res) => {
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const updatedDoc ={
-          $set:{
-            role: "admin"
-          }
-        }
-        const result = await UserCollections.updateOne(filter, updatedDoc);
-        res.send(result);
-      })
-      // --------------- make a user contest creator ---------------
-      app.patch("/users/creator/:id", async (req,res) => {
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const updatedDoc ={
-          $set:{
-            role: "creator"
-          }
-        }
-        const result = await UserCollections.updateOne(filter, updatedDoc);
-        res.send(result);
-      })
-      // --------------- make a user  ---------------
-      app.patch("/users/user/:id", async (req,res) => {
-        const id = req.params.id;
-        const filter = {_id: new ObjectId(id)};
-        const updatedDoc ={
-          $set:{
-            role: "user"
-          }
-        }
-        const result = await UserCollections.updateOne(filter, updatedDoc);
-        res.send(result);
-      })
+
+  // Make a user admin
+  app.patch("/users/admin/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        role: "admin"
+      }
+    };
+    const result = await UserCollections.updateOne(filter, updatedDoc)
+    res.send(result);
+  });
+
+  // Make a user contest creator
+  app.patch("/users/creator/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        role: "creator"
+      }
+    };
+    const result = await UserCollections.updateOne(filter, updatedDoc)
+    res.send(result);
+  });
+
+  // Make a user regular user
+  app.patch("/users/user/:id", async (req, res) => {
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) };
+    const updatedDoc = {
+      $set: {
+        role: "user"
+      }
+    };
+    const result = await UserCollections.updateOne(filter, updatedDoc)
+    res.send(result);
+  });
 
   // Ping to confirm a successful connection
-  client.db("admin").command({ ping: 1 }).then(() => {
-    console.log("Pinged MongoDB successfully!");
-  }).catch(err => {
-    console.error("Error pinging MongoDB:", err);
-    process.exit(1);
-  });
+  client.db("admin").command({ ping: 1 })
+    .then(() => {
+      console.log("Pinged MongoDB successfully!");
+    })
+    .catch(err => {
+      console.error("Error pinging MongoDB:", err);
+      process.exit(1);
+    });
 }).catch(err => {
   console.error("Error connecting to MongoDB:", err);
   process.exit(1);
